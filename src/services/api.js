d00a5579a7c5f20844791ed77c9b41331197145a
jonathan.oliveira <jonathan.oliveira@DEV-JONATHAN>
2020-03-10 17:48:54 -0300

    Remove unnecessary axios interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -4,15 +4,4 @@ const api = axios.create({
 	baseURL: 'http://jsonplaceholder.typicode.com'
 });
 
-// Add a request interceptor
-api.interceptors.request.use(
-	config => {
-		config.headers.grupoId = 1;
-		return config;
-	},
-	error => {
-		return Promise.reject(error);
-	}
-);
-
 export default api;
